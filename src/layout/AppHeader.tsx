9e4a66fc782c503066653074cd3d2008537c0d74
Reza Aditya Sutrisno <reza.aditya@pelni.co.id>
2025-07-28 07:35:21 +0700

    update header

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { ThemeToggleButton } from "@/components/common/ThemeToggleButton";
-import NotificationDropdown from "@/components/header/NotificationDropdown";
-import UserDropdown from "@/components/header/UserDropdown";
+// import NotificationDropdown from "@/components/header/NotificationDropdown";
+// import UserDropdown from "@/components/header/UserDropdown";
 import { useSidebar } from "@/context/SidebarContext";
 import Image from "next/image";
 import Link from "next/link";
@@ -165,11 +165,11 @@ const AppHeader: React.FC = () => {
             <ThemeToggleButton />
             {/* <!-- Dark Mode Toggler --> */}
 
-           <NotificationDropdown /> 
+           {/* <NotificationDropdown />  */}
             {/* <!-- Notification Menu Area --> */}
           </div>
           {/* <!-- User Area --> */}
-          <UserDropdown /> 
+          {/* <UserDropdown />  */}
     
         </div>
       </div>
